cb6402f029fbfbefaeb58755c16636b54bead460
igorlialko <igorlialko8@gmail.com>
2024-01-19 10:48:12 +0200

    fix: admin controllers for blog

--- client/dist/blogs/blogs.controller.d.ts
+++ client/dist/blogs/blogs.controller.d.ts
@@ -1,13 +1,10 @@
 import { BlogsService } from './blogs.service';
-import { CreateBlogDto } from './dto/create-blog.dto';
 import { PaginationDto } from "./dto/pagination.dto";
 import { Blog } from "./model/blog.model";
 import { SlugDto } from "./dto/slug.dto";
 export declare class BlogsController {
     private readonly blogsService;
     constructor(blogsService: BlogsService);
-    create(createBlogDto: CreateBlogDto, image: any): Promise<any>;
     findAll(paginationDto: PaginationDto): Promise<Blog[]>;
     findOne(params: SlugDto): Promise<any>;
-    removeAll(): Promise<string>;
 }
